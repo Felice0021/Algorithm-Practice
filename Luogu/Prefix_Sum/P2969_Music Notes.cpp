e0d662769aed32412ca3e174101c721352666081
Felice <2447976161@qq.com>
2026-04-04 15:53:41 +0800

    Create P2969_Music Notes.cpp

---
+++ Luogu/Prefix_Sum/P2969_Music Notes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 5e4 + 10;
+
+int n, q;
+int f[N]; // 前缀和数组
+
+int main(){
+    cin >> n >> q;
+    for(int i=1; i<=n; i++){
+        int x; cin >> x;
+        f[i] = f[i-1] + x;
+    }
+
+    while(q--){
+        int t; cin >> t;
+        int l = 1, r = n;
+        while(l<r){
+            int mid = (l + r) / 2;
+            if(f[mid]>t) r = mid;
+            else l = mid + 1;
+        }
+        cout << l << endl;
+    }
+
+    return 0;
+}
