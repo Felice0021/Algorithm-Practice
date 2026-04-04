dcd0e034c2f6176ec503ca1a109dc38b152927c8
Felice <2447976161@qq.com>
2026-04-04 15:52:01 +0800

    Create P7071_优秀的拆分(二进制).cpp

---
+++ Luogu/Simulation/P7071_优秀的拆分(二进制).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    if(n&1) cout << -1 << endl;
+    else{
+        for(int i=32; i>=0; i--){
+            if((n>>i)&1) cout << (1<<i) << " ";
+        }
+    }
+
+    return 0;
+}
